57c2c43cb01a738cda973532fb267832d433cdd1
hcminh90 <hcminh90@gmail.com>
2019-11-17 16:48:07 +0700

    minhhc update

--- SQL/Insert data.sql
+++ SQL/Insert data.sql
@@ -2,4 +2,4 @@ insert into warehousecms.roles(role_name, role_desc) values("ADMIN","Administrat
 
 insert into warehousecms.roles(role_name, role_desc) values("KETOAN","Kế toán");
 
-insert into warehousecms.users(role_id, name, status) values(1,"Admin","ENABLED");
+insert into warehousecms.users(role_id, user_name, name, status, password, salt) values(1,"Admin","Admin","ENABLED","11A75DUOWEnRx3fKAvQ/XxrRkSH7T3C6QBYPmOLw5Ex7W8tJfHWmuA==", "vBVy0jBQDusut+ducA4jDFvS3C8i0R7120/8CxlAcA5A37fpZmxSCQ==");  
